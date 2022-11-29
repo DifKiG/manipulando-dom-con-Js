3df6e54a72a6ac94ba6c9743df1dd5d53c5098ec
Diego <diegokijano@hotmail.com>
2022-11-29 00:44:25 -0500

    clase04 completada

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
-const btn = document.querySelector("[data-form-btn]");
-//Arrow function o funciones anonimas "const createTask = (evento) => {""
-const createTask = (evento) => {
-    evento.preventDefault()
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card")
-    input.value = " ";
-    const content = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
-
-    list.appendChild(task);
-    
-    console.log(content);
-};
-//listener
-    console.log(btn);
-    
-    btn.addEventListener("click", createTask );
-
-    const checkComplete = () => {
-        const i = document.createElement("i")
-        i.classList.add ("far fa-check-square icon")
-    }
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+    const btn = document.querySelector("[data-form-btn]");
+    //Arrow function o funciones anonimas "const createTask = (evento) => {""
+    const createTask = (evento) => {
+        evento.preventDefault()
+        const input = document.querySelector("[data-form-input]");
+        const value = input.value;
+        const list = document.querySelector("[data-list]");
+        const task = document.createElement("li");
+        task.classList.add("card")
+        input.value = " ";
+        const taskContent = document.createElement("div");
+        const titleTask = document.createElement("span");
+        titleTask.classList.add("task");
+        titleTask.innerText = value;
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(titleTask);
+        //task.innerHTML = content;
+        task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
+        list.appendChild(task);
+    };
+    //listener
+    btn.addEventListener("click", createTask);    
